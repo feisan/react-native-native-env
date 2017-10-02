3f1962544f4f695e040d919dd47eeee9938000c2
Kelvin Liang <feisan@outlook.com>
2017-10-02 12:48:34 -0300

    Remove createJSModules @overide marker - RN 0.47 compatibility

--- android/src/main/java/com/xgfe/reactnativeenv/RCTNativeEnvPackage.java
+++ android/src/main/java/com/xgfe/reactnativeenv/RCTNativeEnvPackage.java
@@ -31,7 +31,7 @@ public class RCTNativeEnvPackage implements ReactPackage {
         );
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
